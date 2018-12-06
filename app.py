5264d6709e64a23cfe3afe6a3a7dde47d9f15109
Savagecoder77 <savagecoder77@gmail.com>
2018-12-05 20:59:24 -0800

    add simple base code(CLI needed to run it)

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
 # Imports
+from flask_appbuilder import AppBuilder, expose, BaseView
+from app import appbuilder
+
+class MyView(BaseView):
+    route_base = "/myview"
+
+    @expose('/method1/<string:param1>')
+    def method1(self, param1):
+        # do something with param1
+        # and return it
+        return param1
+
+    @expose('/method2/<string:param1>')
+    def method2(self, param1):
+        # do something with param1
+        # and render it
+        param1 = 'Hello %s' % (param1)
+        return param1
+
+appbuilder.add_view_no_menu(MyView())
