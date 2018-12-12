70a1bbd8b7280f16cd1e62b3f63aaac445d4084b
Savagecoder77 <savagecoder77@gmail.com>
2018-12-11 20:33:14 -0800

    move code to views.py

--- lowell-dashboard/app/models.py
+++ lowell-dashboard/app/models.py
@@ -2,8 +2,6 @@ from flask_appbuilder import Model
 from flask_appbuilder.models.mixins import AuditMixin, FileColumn, ImageColumn
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from flask_appbuilder.security.registerviews import RegisterUserDBView
-
 
 """
 
@@ -13,16 +11,3 @@ AuditMixin will add automatic timestamp of created and modified by who
 
 
 """
-
-# Create Registration system
-class MyRegisterUserDBView(RegisterUserDBView):
-    email_template = 'register_mail.html'
-    email_subject = lazy_gettext('Your Account activation for Lowell Help Forum')
-    activation_template = 'activation.html'
-    form_title = lazy_gettext('Fill out the registration form')
-    error_message = lazy_gettext('Not possible to register you at the moment, try again later')
-    message = lazy_gettext('Registration sent to your email')
-
-# Change roles
-class MySecurityManager(SecurityManager):
-    registeruserdbview = MyRegisterUserDBView
