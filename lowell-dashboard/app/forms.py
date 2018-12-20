c6fae15d544739efc3292f5a9a863933b83d7a5b
Savagecoder77 <savagecoder77@gmail.com>
2018-12-20 10:28:59 -0800

    add default form

---
+++ lowell-dashboard/app/forms.py
@@ -0,0 +1,19 @@
+from wtforms import Form, StringField
+from wtforms.validators import DataRequired
+from flask_appbuilder.fieldwidgets import BS3TextFieldWidget
+from flask_appbuilder.forms import DynamicForm
+
+
+class MyForm(DynamicForm):
+
+    field1 = StringField(('Field1'),
+                          description=('Your field number one!'),
+                          validators = [DataRequired()],
+                          widget=BS3TextFieldWidget()
+                          )
+
+    field2 = StringField(('Field2'),
+                          description=('Your field number two!'),
+                          validators = [DataRequired()],
+                          widget=BS3TextFieldWidget()
+                          )
