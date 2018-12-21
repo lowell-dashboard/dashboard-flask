0fc48f637e6cf3fce618ec24112ccf0725b0b770
Savagecoder77 <savagecoder77@gmail.com>
2018-12-20 17:59:25 -0800

    change form fields

--- lowell-dashboard/app/forms.py
+++ lowell-dashboard/app/forms.py
@@ -4,16 +4,18 @@ from flask_appbuilder.forms import DynamicForm
 from flask_appbuilder.fieldwidgets import BS3TextFieldWidget
 
 
-class bugreport(DynamicForm):
+class bugreportform(DynamicForm):
 
-    field1 = StringField(('Field1'),
-                          description=('Your field number one!'),
-                          validators = [DataRequired()],
+    field1 = StringField(('Name'),
+                          description=('Not Required'),
                           widget=BS3TextFieldWidget()
                           )
-
-    field2 = StringField(('Field2'),
-                          description=('Your field number two!'),
+    field2 = StringField(('Email'),
+                          description=('Not Required'),
+                          widget=BS3TextFieldWidget()
+                          )
+    field3 = StringField(('Bug'),
+                          description=('Put the bug report here'),
                           validators = [DataRequired()],
                           widget=BS3TextFieldWidget()
                           )
