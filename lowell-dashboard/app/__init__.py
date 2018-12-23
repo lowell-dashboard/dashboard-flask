5ca5fb6d746786f389cf3ad88bb71758996c0fd9
Savagecoder77 <savagecoder77@gmail.com>
2018-12-22 17:44:59 -0800

    remove uneeded code

--- lowell-dashboard/app/__init__.py
+++ lowell-dashboard/app/__init__.py
@@ -1,12 +1,13 @@
 import logging
 from flask import Flask
-from flask_appbuilder import SQLA, AppBuilder
 from app.index import MyIndexView
 from .security import MySecurityManager
+from flask_appbuilder import SQLA, AppBuilder
+
+
 """
  Logging configuration
 """
-
 logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 logging.getLogger().setLevel(logging.DEBUG)
 
@@ -14,19 +15,5 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLA(app)
 appbuilder = AppBuilder(app, db.session, indexview=MyIndexView, security_manager_class=MySecurityManager)
-
-
-"""
-from sqlalchemy.engine import Engine
-from sqlalchemy import event
-
-#Only include this for SQLLite constraints
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    # Will force sqllite contraint foreign keys
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
-"""
 
 from app import views
