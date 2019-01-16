15262bbf4b6060e495c6b6294205c9a5f5ed4f16
Savagecoder77 <savagecoder77@gmail.com>
2019-01-15 22:11:01 -0800

    add new route for future

--- lowell-dashboard/app/views.py
+++ lowell-dashboard/app/views.py
@@ -5,7 +5,7 @@ from flask import render_template, flash, g, make_response, current_app, abort
 from secret import SLACK
 from .forms import bugreportform, CreateNews
 from requests import post
-from datetime import datetime
+#from tools.news import
 from app.tools import retrieve_schedule
 from app.tools import wkmonth
 from app.models import NewsPost
@@ -37,8 +37,11 @@ class LowellResources(BaseView):
     that contains any added news might be moved to models to
     work with database and can only be seen by logged in users
     '''
-    @expose('/news')
-    def newsview(self):
+    @expose('/news/<number>')
+    def newsview(self, number='1'):
+        if number == '0':
+            number = '1'
+        num = int(number)
         num_news = 1
         # Get data from db
         news_db_data = db.session.query(NewsPost).order_by(NewsPost.id).all()
@@ -48,66 +51,7 @@ class LowellResources(BaseView):
         for news_posts in reversed(news_db_data):
             news_data.append(news_posts)
 
-        # fill random object because jinja starts index at 1
-        how_long_ago = ['lowell help forum filler bot to help jinja yay']
-        time_unit = ['lowell help forum filler bot to help jinja yay']
-        # Get current Time
-        now = datetime.now()
 
-        # Code to display how long ago code was created
-        for times in news_data:
-            # Get time when post was created (datetime object)
-            then = times.time_created
-            # Calculate delta time (how much time between now and when post was created)
-            delta = now - then
-            # Get delta days (days from post creation)
-            delta_days = delta.days
-            # Get delta seconds (seconds from post creation)
-            # datetime only has delta data on days, seconds, and micro seconds
-            # Must multiply seconds to find minutes and hour
-            delta_seconds = delta.seconds
-
-            # If post was create over 1 day before
-            if delta_days != 0:
-                # set number shown as delta days
-                time_ago = delta_days
-                # If over 1 day use 'days' else just 'day'
-                if delta_days == 1:
-                    time_measure = 'day'
-                else:
-                    time_measure = 'days'
-            # Post was not created over a day ago
-            else:
-                # Check if post was created a minute or more ago
-                if delta_seconds >= 60:
-                    # Check if post was created a hour or more ago
-                    if delta_seconds >= 3600:
-                        # Set number shown as delta seconds divided by 3600(seconds in hour) and made into a integer
-                        temp_time = delta_seconds // 3600
-                        time_ago = int(temp_time)
-                        # If over 1 hour use 'hours' else just 'hour'
-                        if time_ago == 1:
-                            time_measure = 'hour'
-                        else:
-                            time_measure = 'hours'
-                    # Post was created minutes ago less than a hour
-                    else:
-                        # Set number shown as delta seconds divided by 60(seconds in minute) and made into a integer
-                        temp_time = delta_seconds // 60
-                        time_ago = int(temp_time)
-                        # If over 1 minute use 'minutes' else just 'minute'
-                        if time_ago == 1:
-                            time_measure = 'minute'
-                        else:
-                            time_measure = 'minutes'
-                # Post was created seconds ago less than a minute
-                else:
-                    # set number shown as delta seconds
-                    time_ago = delta_seconds
-                    # set time unit as seconds since the likely hood of seeing a post after just 1 second is near impossible
-                    time_measure = 'seconds'
-            how_long_ago.append(time_ago)
-            time_unit.append(time_measure)
         return self.render_template('news.html', news=news_data, timestamps=how_long_ago, time_unit=time_unit)
 
     '''
@@ -226,6 +170,23 @@ class SEOfiles(BaseView):
 
 # Create paths
 appbuilder.add_view_no_menu(SEOfiles())
+
+# Views for SEO Site files
+class UserInfo(BaseView):
+
+    # Add route base as root "/"
+    route_base = "/"
+
+    '''
+    Create path robots.txt that renders robots.txt text file
+    that contains project's robots.txt paths
+    '''
+    @expose('/profile/<user>')
+    def disclaimer(self, user):
+        return render_template('profile.html')
+
+# Create paths
+appbuilder.add_view_no_menu(UserInfo())
 
 '''
 Form Views
