60ab0b5a2bc38694b784a0fcea9b911f3064b827
Savagecoder77 <savagecoder77@gmail.com>
2019-01-17 12:49:46 -0800

    add code

--- lowell-dashboard/app/tools/news.py
+++ lowell-dashboard/app/tools/news.py
@@ -1,19 +1,40 @@
 from datetime import datetime
 from json import loads
 
+from datetime import datetime
+
 class NewsWork:
     def __init__(self, list):
         self.list = list
 
-    def news_time(self)
+    def news_sort(self):
+        news_data = {'news_data':{}}
+        list_length = int(len(self.list))
+        count = 0
+        for number in range(0, list_length):
+            if self.check_ten(number) == True:
+                count += 1
+                news_data['news_data'][str(count)] = {'time_created_list':[],'time_measure':[],'new_post':[]}
+
+        final_news_data = self.time_sort(news_data)
+        return final_news_data
+
+    def time_sort(self, dic):
         # fill random object because jinja starts index at 1
         how_long_ago = ['lowell help forum filler bot to help jinja yay']
         time_unit = ['lowell help forum filler bot to help jinja yay']
         # Get current Time
         now = datetime.now()
+        # Keeping dictionary count
+        count = 1
+        # Keeping Loop count
+        loop_count = 0
 
         # Code to display how long ago code was created
         for times in self.list:
+            loop_count += 1
+            if self.check_ten(loop_count) == True:
+                count += 1
             # Get time when post was created (datetime object)
             then = times.time_created
             # Calculate delta time (how much time between now and when post was created)
@@ -86,18 +107,28 @@ class NewsWork:
                     time_ago = delta_seconds
                     # set time unit as seconds since the likely hood of seeing a post after just 1 second is near impossible
                     time_measure = 'seconds'
-            how_long_ago.append(time_ago)
-            time_unit.append(time_measure)
+            dic['news_data'][str(count)]['time_created_list'].append(time_ago)
+            dic['news_data'][str(count)]['time_measure'].append(time_measure)
 
-        return how_long_ago, time_unit
+        return dic
 
-    def news_sort(self):
-        news_data = {'news_data':{}}
-        for number in len(self.list):
-            if self.check_ten(number) == True:
-                news_data['userdata'][str(number)] = {}
-        for items in self.list:
-            pass
+    def news_data_sort(self, dic):
+        # dictionary count
+        count = 1
+        # loop count
+        loop_count = 0
+        # loop for all items in list
+        for news in self.list:
+            # count loops
+            loop_count += 1
+            # loop looped multiple of 10
+            if self.count_ten(loop_count) == True:
+                # change data output to diffent page
+                count += 1
+            # add db object to proper lists
+            dic['news_data'][str(count)]['new_post'].apphend(news)
+
+        return dic
 
     def check_ten(self, number):
         var_num = number % 10
