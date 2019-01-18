81c58fa44d8e757e20439679baebf66624214c29
calee14 <caplee1000@gmail.com>
2019-01-17 18:48:57 -0800

    adding back jinja code to profile page

--- lowell-dashboard/app/templates/profile.py
+++ lowell-dashboard/app/templates/profile.py
@@ -1,5 +1,11 @@
 {% extends "appbuilder/base.html" %}
 
+{% block head_meta %}
+  <title>Profile Page</title>
+  <meta name="description" content="Profile">
+  <meta charset='utf-8'>
+{% endblock %}
+
 {% block content %}
 
 {% endblock %}
