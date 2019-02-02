72f30a086f856067a30b0f4f77816833148ea1e0
calee14 <caplee1000@gmail.com>
2019-02-02 10:26:44 -0800

    adding class data

--- lowell-dashboard/testdata.py
+++ lowell-dashboard/testdata.py
@@ -7,3 +7,15 @@ q = db.session.query(CustomUser).get((1))
 q.class_ids = ['1', '3', '4', '5']
 print(q.class_ids)
 db.session.commit()
+
+classes = Classes()
+classes.teacher = 'Mr. Simon'
+classes.course_name = 'Geometry'
+classes.student_grade_levels = '9th'
+classes.block_number = 3
+classes.year = 2019
+classes.course_type = 'Math'
+classes.a_g_requirement = 'A'
+classes._students_ids = ''
+db.session.add(classes)
+db.session.commit()
