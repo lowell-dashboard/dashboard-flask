071e763c119c5f59ac3b774331ce77e6adabc93f
calee14 <caplee1000@gmail.com>
2019-05-11 16:57:00 -0700

    implementing the schedule service

--- lowell-dashboard/app/views.py
+++ lowell-dashboard/app/views.py
@@ -7,7 +7,7 @@ from secret import SLACK
 from .forms import bugreportform, CreateNews
 from requests import post
 from app.tools import retrieve_schedule
-from app.tools import wkmonth
+from app.tools.wkmonth import ScheduleService
 from app.models import NewsPost, CustomUser, Classes
 from flask_babel import lazy_gettext as _
 from flask_appbuilder import ModelView, AppBuilder, BaseView, expose, has_access, SimpleFormView, PublicFormView
@@ -76,10 +76,11 @@ class LowellResources(BaseView):
     def schedules(self, type):
         # uncomment if you need to update the schedule json
         # retrieve_schedule.update_schedule()
-        codes = wkmonth.week_of_month()
+        schedule_service = ScheduleService()
+        codes = schedule_service.week_of_month()
         # print(codes)
-        schedule_data = wkmonth.get_schedule_times(codes)
-        # print(wkmonth.get_week_events())
+        schedule_data = schedule_service.get_schedule_times(codes)
+        print(schedule_service.get_week_events())
         if type == 'day':
             pass
         return self.render_template(
